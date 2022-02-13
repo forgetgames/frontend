b19b69b9775d3065e37314827aaf38d4809d249f
Zac Pez <zacpez@gmail.com>
2022-02-13 12:27:37 -0800

    added deploy as script, and updated server status values

--- src/components/organisms/card/card.vue
+++ src/components/organisms/card/card.vue
@@ -9,12 +9,12 @@ defineProps(['server'])
       <span class="ml-auto p-1 bg-white rounded-4xl">
         <span class="px-2 border-r-1 border-black"><carbon-user-filled class="align-text-top" />{{ server.playerCount }}</span>
         <span class="mx-2 rounded-xl text-lime-800">
-          <carbon-checkmark-outline v-if="server.status === 'online'" class="align-text-top" />
-          <carbon-restart v-else-if="server.status === 'restarting'" class="align-text-top animate-spin-slow" />
-          <carbon-pause-outline v-else-if="server.status === 'offline'" class="align-text-top" />
-          <carbon-data-check v-else-if="server.status === 'archive'" class="align-text-top" />
-          <carbon-data-error v-else-if="server.status === 'deleted'" class="align-text-top" />
-          <carbon-migrate v-else-if="server.status === 'migrating'" class="align-text-top" />
+          <carbon-checkmark-outline v-if="server.status === 'ONLINE'" class="align-text-top" />
+          <carbon-restart v-else-if="server.status === 'RESTARTING'" class="align-text-top animate-spin-slow" />
+          <carbon-pause-outline v-else-if="server.status === 'OFFLINE'" class="align-text-top" />
+          <carbon-data-check v-else-if="server.status === 'archived'" class="align-text-top" />
+          <carbon-data-error v-else-if="server.status === 'DELETED'" class="align-text-top" />
+          <carbon-migrate v-else-if="server.status === 'MIGRATING'" class="align-text-top" />
           <carbon-warning-hex v-else />
 
         </span>
