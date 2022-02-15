eb1534bee0dd7ce96132da31f29d0ef8396a4257
Zac Pez <zacpez@gmail.com>
2022-02-15 08:17:07 -0800

    adds constants file

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export class Status {
+  static ARCHIVED = 'ARCHIVED'
+  static DELETED = 'DELETED'
+  static MIGRATING = 'MIGRATING'
+  static ONLINE = 'ONLINE'
+  static OFFLINE = 'OFFLINE'
+  static RESTARTING = 'RESTARTING'
+}
