acfafe32fb1f8c77d970e22dc27203dff632c9d6
Zac Pez <zacpez@gmail.com>
2022-02-19 09:47:17 -0800

    clean up build warnings

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,21 @@ import {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset')
+                atRule.remove()
+            },
+          },
+        },
+      ],
+    },
+  },
   resolve: {
     alias: {
       '~': `${path.resolve(__dirname, './src/')}`,
